b3d30277b5378a28295ad017eacbf3be11debe9c
petrubear <edison.martinez@gmail.com>
2023-09-14 20:02:20 -0500

    feat: add pino configutation

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
+import { LoggerModule } from 'nestjs-pino';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [
+    LoggerModule.forRoot({
+      pinoHttp: {
+        level: 'debug',
+        transport: {
+          target: 'pino-pretty',
+          options: {
+            singleLine: true,
+            ignore: 'pid,hostname',
+          },
+        },
+      },
+    }),
+    ProductsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
